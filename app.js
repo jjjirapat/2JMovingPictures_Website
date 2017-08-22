6c0c72aa9c27f4e96118fdc65d5f79a9011d32a4
jsangva <jsangva@ncsu.edu>
2017-08-22 13:14:25 -0400

    Boiler setup

--- app.js
+++ app.js
@@ -20,4 +20,15 @@
 			canPurchase: false,
 		}
 	];
+
+	app.controller("PanelController", function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
+	});
 })();
