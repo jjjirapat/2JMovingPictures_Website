69bc8504dd3239706acd96c42b36fc3410157242
jsangva <jsangva@ncsu.edu>
2017-08-23 18:23:16 -0400

    imagetest

---
+++ imagetest.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- Meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+		<!-- jQuery, Tether, Popper, Boostrap JS -->
+		<script src="jquery-3.2.1.min.js" type="text/javascript"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
+		<!-- Other Scripts -->
+		<script type="text/javascript">
+			//Onload function
+			$(document).ready(function() {
+				//$("#logofade").addClass("load");
+			});
+
+		</script>
+
+		<!-- CSS -->
+		<style type="text/css">
+			#logofade {
+				width: 400px;
+				margin-top: 200px;
+				-webkit-transition: all 2s;
+				transition: all 2s;
+			}
+
+			.load {
+			}
+		</style>
+
+	</head>
+	<body>
+		<div style="text-align: center; ">
+			<img src="images/logo-color-full.jpg" id="logofade">
+		</div>
+
+	</body>
+</html>
