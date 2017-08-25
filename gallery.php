e1beeb9a1317895fd683aee768f361aacca9d2b8
jsangva <jsangva@ncsu.edu>
2017-08-25 12:39:32 -0400

    Update for walking animation

--- gallery.php
+++ gallery.php
@@ -26,21 +26,23 @@
 				});
 
 			function walkAnimate() {
-				var rng = Math.floor(((Math.random() * 10) + 1)/2);
-				if(rng == 1) {
-					$('.person', this)
+				var x = Math.floor((Math.random() * 10) + 1);
+				var id = Math.floor(((Math.random() * 10) + 1)/2);
+				var rng = Math.floor((Math.random() * 100) + x);
+				if(0 < rng >= 20) {
+					$('#person'+id)
 						.delay(1000)
 						.animate({ left: "+=10px", top: "+=10px" }, '50');
-				}else if(rng == 2) {
-					$(this)
+				}else if(20 < rng >= 40) {
+					$('#person'+id)
 						.delay(1000)
 						.animate({ left: "+=10px", top: "-=10px" }, '50');
-				}else if(rng == 3) {
-					$(this)
+				}else if(40 < rng >= 80) {
+					$('#person'+id)
 						.delay(1000)
 						.animate({ left: "-=10px", top: "+=10px" }, '50');
-				}else if(rng == 4) {
-					$(this)
+				}else if(80 < rng >= 100) {
+					$('#person'+id)
 						.delay(1000)
 						.animate({ left: "-=10px", top: "-=10px" }, '50');
 				}else if(rng == 5) {
@@ -83,10 +85,11 @@
 			<h1>
 				Welcome to the Gallery!
 			</h1>
-			<img class="tossing person" onload="" src="images/commentor.png" style="height: 50px; position: relative;">
-			<img class="tossing person" src="images/commentor.png" style="height: 50px; position: relative;">
-			<img class="tossing person" src="images/commentor.png" style="height: 50px; position: relative;">
-			<img class="tossing person" src="images/commentor.png" style="height: 50px; position: relative;">
+			<img id="person1" class="tossing" src="images/commentor.png" style="height: 50px; position: relative;">
+			<img id="person2" class="tossing" src="images/commentor.png" style="height: 50px; position: relative;">
+			<img id="person3" class="tossing" src="images/commentor.png" style="height: 50px; position: relative;">
+			<img id="person4" class="tossing" src="images/commentor.png" style="height: 50px; position: relative;">
+			<img id="person5" class="tossing" src="images/commentor.png" style="height: 50px; position: relative;">
 		</div>
 	</body>
 </html>
